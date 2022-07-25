22902dcbf8d641af32dd67efe472b58f93b55979
mtauhidul <mislam.tauhidul@gmail.com>
2022-07-25 17:58:49 +0600

    Only rotating div boxes here

---
+++ App.js
@@ -0,0 +1,307 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div className=''>
+          <div className='bg-black text-white rounded-lg p-4 mb-8 relative'>
+            <div className='font-medium text-3xl md:text-4xl uppercase'>
+              Join
+            </div>
+            <div className='border-t py-4 mt-4'>
+              <p>
+                Learn about our benefits, see how to join, and get a sense of
+                what it means to be a member of FWB.
+              </p>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                className='btn font-bold'
+                data-has-icon='true'
+                href='join.html'>
+                Become a member
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-null'
+                  className='is-internal'>
+                  <title id='arrow-null'>Arrow</title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='overflow-hidden transition-all duration-500 max-h-0' />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className=''>
+          <div className='bg-black text-white rounded-lg p-4 mb-8 relative'>
+            <div className='font-medium text-3xl md:text-4xl uppercase'>
+              Opportunities
+            </div>
+            <div className='border-t py-4 mt-4'>
+              <p>
+                Discover ways to collaborate more closely with our community,
+                from open contributor roles to project grants, fellowships, and
+                beyond.
+              </p>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                className='btn font-bold'
+                data-has-icon='true'
+                href='opportunities.html'>
+                View Opportunities
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-null'
+                  className='is-internal'>
+                  <title id='arrow-null'>Arrow</title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='overflow-hidden transition-all duration-500 max-h-0' />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className=''>
+          <div className='bg-black text-white rounded-lg p-4 mb-8 relative'>
+            <div className='home-locked'>
+              <div>75 $FWB</div>
+            </div>
+            <div className='font-medium text-3xl md:text-4xl uppercase'>
+              Discord
+            </div>
+            <div className='border-t py-4 mt-4'>
+              <p>
+                The FWB Discord is where Web3 is evolving in real time, with
+                channels for art, events, NFTs, governance, job opportunities,
+                trading crypto, and so much more.
+              </p>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                href='https://discord.gg/3jCyswsKFn'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='btn font-bold'
+                data-has-icon='true'>
+                Join The Conversation
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-https://discord.gg/3jCyswsKFn'
+                  className='icon'>
+                  <title id='arrow-https://discord.gg/3jCyswsKFn'>Arrow</title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='overflow-hidden transition-all duration-500 max-h-0' />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className=''>
+          <div className='bg-black text-white rounded-lg p-4 mb-8 relative'>
+            <div className='font-medium text-3xl md:text-4xl uppercase'>
+              Works in Progress
+            </div>
+            <div className='border-t py-4 mt-4'>
+              <p>
+                FWB's editorial platform publishes original writing on
+                decentralized technology and the culture surrounding it.
+              </p>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                href='https://fwb.help/wip'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='btn font-bold'
+                data-has-icon='true'>
+                Read WIP
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-https://fwb.help/wip'
+                  className='icon'>
+                  <title id='arrow-https://fwb.help/wip'>Arrow</title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='overflow-hidden transition-all duration-500 max-h-0' />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className=''>
+          <div className='bg-black text-white rounded-lg p-4 mb-8 relative'>
+            <div className='home-locked'>
+              <div>5 $FWB</div>
+            </div>
+            <div className='font-medium text-3xl md:text-4xl uppercase'>
+              Events
+            </div>
+            <div className='border-t py-4 mt-4'>
+              <p>
+                Whether online or IRL, join us for conversations, radio shows,
+                afterparties, and local meetups.
+              </p>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                className='btn font-bold'
+                data-has-icon='true'
+                href='events.html'>
+                See upcoming events
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-null'
+                  className='is-internal'>
+                  <title id='arrow-null'>Arrow</title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='overflow-hidden transition-all duration-500 max-h-0' />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div className='bg-black text-white rounded-lg p-4 mb-8 relative'>
+            <div className='font-medium text-3xl md:text-4xl uppercase'>
+              Press
+            </div>
+            <div className='border-t py-4 mt-4'>
+              <p>Learn more about what we’re up to, as reported by…</p>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                href='https://www.nytimes.com/2022/03/02/technology/friends-with-benefits-crypto-dao.html'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='btn font-bold'
+                data-has-icon='true'>
+                New York Times
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-https://www.nytimes.com/2022/03/02/technology/friends-with-benefits-crypto-dao.html'
+                  className='icon'>
+                  <title id='arrow-https://www.nytimes.com/2022/03/02/technology/friends-with-benefits-crypto-dao.html'>
+                    Arrow
+                  </title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                href='https://www.newyorker.com/culture/infinite-scroll/the-promise-of-daos-the-latest-craze-in-crypto'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='btn font-bold'
+                data-has-icon='true'>
+                New Yorker
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-https://www.newyorker.com/culture/infinite-scroll/the-promise-of-daos-the-latest-craze-in-crypto'
+                  className='icon'>
+                  <title id='arrow-https://www.newyorker.com/culture/infinite-scroll/the-promise-of-daos-the-latest-craze-in-crypto'>
+                    Arrow
+                  </title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                href='https://ra.co/features/3914'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='btn font-bold'
+                data-has-icon='true'>
+                Resident Advisor
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-https://ra.co/features/3914'
+                  className='icon'>
+                  <title id='arrow-https://ra.co/features/3914'>Arrow</title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='border-t uppercase py-4'>
+              <a
+                href='https://anchor.fm/thedefiant/episodes/Weve-Been-Creating-Value-for-Instagram-and-TikTok-With-Very-Little-Actually-Accruing-to-Us-Trevor-McFedries-elu3v1'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='btn font-bold'
+                data-has-icon='true'>
+                Defiant
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-https://anchor.fm/thedefiant/episodes/Weve-Been-Creating-Value-for-Instagram-and-TikTok-With-Very-Little-Actually-Accruing-to-Us-Trevor-McFedries-elu3v1'
+                  className='icon'>
+                  <title id='arrow-https://anchor.fm/thedefiant/episodes/Weve-Been-Creating-Value-for-Instagram-and-TikTok-With-Very-Little-Actually-Accruing-to-Us-Trevor-McFedries-elu3v1'>
+                    Arrow
+                  </title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='overflow-hidden transition-all duration-500 max-h-0' />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className=''>
+          <div className='bg-black-alpha text-white rounded-lg p-4 mb-8 relative'>
+            <div className='font-medium text-3xl md:text-4xl uppercase' />
+            <div className='null uppercase py-4'>
+              <a
+                href='https://www.artblocks.io/token/120000295'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='btn font-bold'
+                data-has-icon='true'>
+                NFT Backdrop: Endless Nameless #295 by Rafaël Rozendaal
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 100 100'
+                  aria-labelledby='arrow-https://www.artblocks.io/token/120000295'
+                  className='icon'>
+                  <title id='arrow-https://www.artblocks.io/token/120000295'>
+                    Arrow
+                  </title>
+                  <polygon points='18.852 0 18.852 17.084 70.838 17.084 0 87.921 12.079 100 82.916 29.162 82.916 81.148 100 81.148 100 0 18.852 0' />
+                </svg>
+              </a>
+            </div>
+            <div className='border-t py-4 mt-4'>
+              <p>
+                <strong className='uppercase font-bold'>Owned by:</strong> Mike
+                - @mikebodge
+              </p>
+            </div>
+            <div className='overflow-hidden transition-all duration-500 max-h-0' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
